18e9345c12b45c9683f2c332a382be50835c23a5
SZB <skyzen888@gmail.com>
2025-10-26 00:05:23 -0700

    Setting up the main methods and fields.

---
+++ src/Contoller/DungeonAdventure.java
@@ -0,0 +1,80 @@
+package Contoller;
+
+import java.util.Scanner;
+
+/**
+ *  This class is the main logic holding class for the Dungeoneer Game
+ *  @author Skyler Z Broussard
+ *  @version 0.0.1  10/20/2025
+ */
+public class DungeonAdventure 
+{	
+	/**
+	 * This field is the main character 
+	 */
+	static Hero myHero;
+	
+	/**
+	 * This field is a scanner for user input.
+	 */
+	static Scanner myUserInput;
+	
+	/**
+	 * This boolean represents if a game is running.
+	 */
+	static boolean myGameStatus;
+	
+	/**
+	 * This field is the dungeon of the current game.
+	 */
+	// static Dungeon myDungeon;
+	
+	
+	/**
+	 * This is the method with the main workflow.
+	 * @param theArgs
+	 */
+	public static void main(final String theArgs[])
+	{
+		startGame();
+		
+		play();
+		
+	}
+	
+	/**
+	 * This method starts creating the required fields for all other functionalities.
+	 */
+	private static void startGame() 
+	{
+		myUserInput = new Scanner(System.in);
+		//myDungeon  = new Dungeon();
+		//myHero = new Hero();
+		myGameStatus = false;
+	}
+	
+	/**
+	 * This method plays the game when called, until myGameStatus becomes false.
+	 */
+	private static void play()
+	{
+		while(myGameStatus)
+		{
+			
+		}
+	}
+	
+	/**
+	 * This method has all of the logic for moving rooms.
+	 * @param theInput
+	 */
+	private static void processMovement(final String theInput)
+	{
+		
+	}
+	
+	public static Hero getHero()
+	{
+		return myHero;
+	}
+}
